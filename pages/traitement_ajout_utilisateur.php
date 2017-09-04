dd5a075da0a1a86b78f7684d58a67beb254fe2c4
Jim29800 <jbouvier1173@gmail.com>
2017-09-04 15:41:34 +0200

    verification mail et pseudo exist

--- pages/traitement_ajout_utilisateur.php
+++ pages/traitement_ajout_utilisateur.php
@@ -6,6 +6,41 @@ $age = htmlspecialchars($_POST['age']);
 $email = htmlspecialchars($_POST['email']);
 $mdp = htmlspecialchars($_POST['password']);
 $mdp_verif = htmlspecialchars($_POST['password_verif']);
+try    
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=annonces_pf;charset=utf8', 'root', 'admin');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+};
+//---------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+
+function verification($connexion, $mail, $pseudo){
+    $sql_verif_mail = sprintf(
+        'SELECT * FROM uti_utilisateur WHERE uti_email = "%s" OR uti_pseudo = "%s"',$mail, $pseudo
+    );
+    try{
+        $result_verif_mail = $connexion->query($sql_verif_mail);
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    };
+    foreach ($result_verif_mail as $key => $value) {
+        if ($mail === $value['uti_email']){
+            echo "<h1>Email déjà utilisé !</h1><br>";
+            return true;
+        }elseif($pseudo === $value['uti_pseudo']){
+            echo "<h1>Pseudo déjà utilisé !</h1><br>";
+            return true;            
+        }
+    }
+    return false;
+};
+//---------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
 
 if (!isset($mdp)) {
     echo "Aucun mot de passe choisi";
@@ -16,15 +51,11 @@ elseif (strlen($mdp) < 8) {
 elseif ($mdp !== $mdp_verif ){
     echo "les mots de passe de correspondent pas";
 }
+elseif (verification($bdd,$email,$pseudo)) {
+    echo "<h2>Erreur lors de la création du compte.</h2><br>";
+}
 else{
-    try    
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=annonces_pf;charset=utf8', 'root', 'admin');
-    }
-    catch (Exception $e)
-    {
-        die('Erreur : ' . $e->getMessage());
-    };
+    $mdp = password_hash($mdp,PASSWORD_DEFAULT);
     $sql = sprintf(
         "INSERT INTO uti_utilisateur (uti_nom, uti_prenom, uti_age, uti_email, uti_pseudo,uti_password) 
         VALUE ('%s', '%s', %d, '%s',  '%s',  '%s')", $nom, $prenom, $age, $email, $pseudo, $mdp
@@ -34,7 +65,7 @@ else{
         $bdd->query($sql);
         echo "<h1>Bienvenue ".$pseudo."</h1>";
         echo "<p>Votre inscription c'est correctemnt effectué</p>";
-    }
+    }   
     catch (Exception $e)
     {
         die('Erreur : ' . $e->getMessage());
